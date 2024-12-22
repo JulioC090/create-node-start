1e05a6abc4039c6ad41865848f81911214e8612a
JulioC090 <juliocziviani@gmail.com>
2024-12-21 23:14:20 -0300

    feat(cli): add script to copy boilerplate files to project directory

--- cli/index.ts
+++ cli/index.ts
@@ -1,1 +1,25 @@
-console.log('Hello world!');
+#!/usr/bin/env node
+
+import fs from 'node:fs';
+import path from 'node:path';
+
+const cliPath = __dirname;
+const isDev = cliPath.includes('cli');
+
+const boilerplatePath = isDev
+  ? path.resolve(__dirname, '..', 'boilerplate') // Desenvolvimento
+  : path.resolve(__dirname, 'boilerplate'); // Produção
+
+const projectPath = isDev
+  ? path.join(process.cwd(), 'test-boilerplate') // Desenvolvimento
+  : process.cwd(); // Produção
+
+if (!fs.existsSync(boilerplatePath)) {
+  console.error(`Erro: A pasta ${boilerplatePath} não existe.`);
+  process.exit(1);
+}
+
+fs.cpSync(boilerplatePath, projectPath, {
+  errorOnExist: true,
+  recursive: true,
+});
